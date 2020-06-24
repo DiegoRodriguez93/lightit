d48601da5acecdf3a2f109931dc075b087a35099
Diego Rodriguez <diegorodriguez93@hotmail.com>
2020-06-24 08:22:45 -0300

    rise

--- src/components/Leftsidebar/Leftsidebar.js
+++ src/components/Leftsidebar/Leftsidebar.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Link} from '@reach/router';
+import React, { useState } from 'react';
+import { Link } from '@reach/router';
 
 import icon1 from './../../../public/images/aberturas.png';
 import icon2 from './../../../public/images/equipamiento.png';
@@ -7,37 +7,36 @@ import icon3 from './../../../public/images/terminaciones.png';
 
 const Leftsidebar = (props) => {
 
-   const [ activo, activar ] = useState('');
+   const [activo, activar] = useState('');
 
+   return (
+      <div className="h-screen fixed bg-white px-3">
 
-   return(
-     <div className="h-screen fixed bg-white px-3">
+         <div className="h-56"></div>
 
-        <div className="h-56"></div>
-        
          <Link onClick={() => activar('aberturas')} to="/aberturas">
             <div className={`${activo == 'aberturas' ? 'bg-gray-300' : ''} hover:text-white hover:bg-gray-300 p-2 rounded cursor-pointer`}>
-            <img src={icon1}  className="h-auto mx-auto w-12" alt="aberturas icon light-it"/>
-            <p className="text-center text-xs leading-none text-gray-700">Aberturas</p>
+               <img src={icon1} className="h-auto mx-auto w-12" alt="aberturas icon light-it" />
+               <p className="text-center text-xs leading-none text-gray-700">Aberturas</p>
             </div>
          </Link>
 
          <Link onClick={() => activar('equipamiento')} to="/equipamiento">
-         <div className={`${activo == 'equipamiento' ? 'bg-gray-300' : ''} hover:text-white hover:bg-gray-300 p-2 rounded cursor-pointer mx-auto`}>
-            <img src={icon2}  className="h-auto mx-auto w-12" alt="aberturas icon light-it"/>
-            <p className="text-center text-xs leading-none text-gray-700">Equipamiento</p>
+            <div className={`${activo == 'equipamiento' ? 'bg-gray-300' : ''} hover:text-white hover:bg-gray-300 p-2 rounded cursor-pointer mx-auto`}>
+               <img src={icon2} className="h-auto mx-auto w-12" alt="aberturas icon light-it" />
+               <p className="text-center text-xs leading-none text-gray-700">Equipamiento</p>
             </div>
          </Link>
 
          <Link onClick={() => activar('terminaciones')} to="/terminaciones">
             <div className={`${activo == 'terminaciones' ? 'bg-gray-300' : ''} hover:text-white hover:bg-gray-300 p-2 rounded cursor-pointer mx-auto`}>
-            <img src={icon3}  className="h-auto mx-auto w-12" alt="aberturas icon light-it"/>
-            <p className="text-center text-xs leading-none text-gray-700">Terminaciones</p>
+               <img src={icon3} className="h-auto mx-auto w-12" alt="aberturas icon light-it" />
+               <p className="text-center text-xs leading-none text-gray-700">Terminaciones</p>
             </div>
          </Link>
 
-     </div>
-        
+      </div>
+
    )
 
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import arrowDown from './../../../public/images/arrowDown.png';
 
 import logo from './../../../public/images/logo.png';
@@ -9,28 +9,28 @@ const Navbar = () => {
 
    const toggleDropdown = () => {
 
-       hidden == 'hidden' ? openDropdown('') : openDropdown('hidden');
- 
-    }
+      hidden == 'hidden' ? openDropdown('') : openDropdown('hidden');
 
-   return(
+   }
+
+   return (
       <div className="py-1 bg-gray-900 text-sm ">
-   <img src={logo} className="w-40 h-20 ml-16 h-auto inline-block" alt=""/>
-     <div className="mr-16 px-8 pt-4 float-right"> 
+         <img src={logo} className="w-40 h-20 ml-16 h-auto inline-block" alt="" />
+         <div className="mr-16 px-8 pt-4 float-right">
 
-         <button onClick={toggleDropdown}
-         className="bg-white rounded py-2 px-4">
-            Guardar y salir
-         <img 
-         className="w-6 inline-block" 
-         src={arrowDown} />
-         </button>
+            <button onClick={toggleDropdown}
+               className="bg-white rounded py-2 px-4">
+               Guardar y salir
+            <img
+                  className="w-6 inline-block"
+                  src={arrowDown} />
+            </button>
 
-         <div className={`mt-2 py-2 px-2 bg-white text-center rounded-lg shadow-xl absolute z-50 ${ hidden == 'hidden' ? '' : 'hidden' }`}>
-            <a href="#" className="block py-2 text-gray-800 hover:bg-gray-500 hover:text-white">Salir sin guardar</a>
-            <a href="#" className="block py-2 text-gray-800 hover:bg-gray-500 hover:text-white">Guardar y continuar</a>
+            <div className={`mt-2 py-2 px-2 bg-white text-center rounded-lg shadow-xl absolute z-50 ${hidden == 'hidden' ? '' : 'hidden'}`}>
+               <a href="#" className="block py-2 text-gray-800 hover:bg-gray-500 hover:text-white">Salir sin guardar</a>
+               <a href="#" className="block py-2 text-gray-800 hover:bg-gray-500 hover:text-white">Guardar y continuar</a>
+            </div>
          </div>
-      </div> 
 
       </div>
    )
